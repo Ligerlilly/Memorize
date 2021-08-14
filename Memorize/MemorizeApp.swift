6163b413d6c1431d5b62efb30e48c22f4d08c029
Jason Lane <2266577+Ligerlilly@users.noreply.github.com>
2021-08-14 14:09:36 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jason Lane on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
